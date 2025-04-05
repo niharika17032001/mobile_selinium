502ecfffe36b1c4182c64b67d3f390c5fcb8a4ea
niharika17032001 <niharika17032001@gmail.com>
2025-04-05 16:57:33 +0530

    Create main.yml

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import crediantials
+from insta_login import get_instagram_links, save_links_to_json
 from setup_browser import launch_browser
 
 
 def main():
-    browser = "chrome"
-    # browser = "firefox"
-    driver = launch_browser(browser)
+    username = crediantials.USER
+    password = crediantials.PWD
+    target_username = "tamannaahspeaks"
+
+    post_links = get_instagram_links(username, password, target_username, 3)
+    save_links_to_json(post_links)
+    print(f"Extracted {len(post_links)} links saved to instagram_links.json")
 
 if __name__ == "__main__":
     main()
